e03335cc9d947f226987719ecc4358ed7bb05322
18037522 <Chuat74@gmaol.com>
2020-05-04 11:10:47 +0800

    .

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,9 +21,30 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv =  findViewById(R.id.LvYear);
 
         al = new ArrayList<String>();
         al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
+
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,al);
+        lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String year = al.get(i);
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("year",year);
+                startActivity(intent);
+
+
+
+            }
+        });
+
+
 
 
 
