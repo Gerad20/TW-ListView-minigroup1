9a562c78669718edb6aecee45a72e8ef0fc342dd
18016549 <roy1409@outlook.sg>
2020-05-04 11:55:56 +0800

    i want to commit

---
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/Module.java
@@ -0,0 +1,20 @@
+package com.myapplicationdev.android.tw_listview;
+
+public class Module {
+    private String name;
+    private int image;
+
+    public Module(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+}
--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -1,11 +1,43 @@
 package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ModuleAdapter {
+import java.util.ArrayList;
 
+public class ModuleAdapter extends ArrayAdapter<Module> {
+
+    private ArrayList<Module> Module;
     private Context context;
+    private TextView tvModule;
+    private ImageView ivModule;
 
+    public ModuleAdapter(Context context, int resource, ArrayList<Module> objects) {
+        super(context, resource, objects);
+        Module = objects;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.modulerow, parent, false);
+        tvModule = (TextView)rowView.findViewById(R.id.tvModule);
+        ivModule = (ImageView)rowView.findViewById(R.id.ivModule);
+
+        Module currentmodule = Module.get(position);
+        tvModule.setText(currentmodule.getName());
+        ivModule.setImageResource(currentmodule.getImage());
+
+        return rowView;
+
+    }
 
 
 
